b7b11d95d90a0fb980194fd077acdf66b17ee178
Mahreen Azam <Mahreen.azam@willowtreeapps.com>
2017-03-10 15:58:49 -0500

    Update javascript-sample.js

--- javascript-sample.js
+++ javascript-sample.js
@@ -1,27 +1,3 @@
-/*((state, timeLeftFn) => {
-    const turnDirections = ['right', 'left', 'about-face'];
-    const turnDirection = turnDirections[Math.floor(Math.random() * 3)];
-
-    const smokeDirections = ['forward', 'backward', 'left', 'right', 'drop'];
-    const smokeDirection = smokeDirections[Math.floor(Math.random() * 5)];
-
-    const index = Math.floor(Math.random() * 17);
-    const command = index < 10 ?
-        { action: 'move', metadata: {} } :
-        index < 12 ?
-            { action: 'turn', metadata: { direction: turnDirection } } :
-            index < 16 ?
-                { action: 'shoot', metadata: {} } :
-                { action: 'smoke', metadata: { direction: smokeDirection } };
-
-    return {
-        command,
-        state: {
-            hello: 'world'
-        }
-    };
-}); */
-
 ((state, timeLeftFn) => {
 
    var counter = state['saved-state'].counter || 0;
@@ -37,10 +13,20 @@
    }
    else if (counter === 1)
    {
+        command = { action: 'move', metadata: {} };
+        counter++;
+   }
+    else if (counter === 2)
+   {
+        command = { action: 'move', metadata: {} };
+        counter++;
+   }
+   else if (counter === 3)
+   {
         command = { action: 'smoke', metadata: {} };
         counter++;
    }
-   else if (counter === 2)
+   else if (counter === 4)
    {
         command = { action: 'turn', metadata: {direction: turnDirection} };
         counter++;
@@ -58,5 +44,3 @@
         }
     };
 });
-
-
